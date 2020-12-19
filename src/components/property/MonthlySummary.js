c267326fcf2ebf071e3e51d4f04fb6c7de58d64b
Stephan <stephanglobals@gmail.com>
2020-12-19 14:22:47 +0800

    montly estimation added

--- src/components/property/MonthlySummary.js
+++ src/components/property/MonthlySummary.js
@@ -1,18 +1,38 @@
 import React from 'react'
 
-export default function MonthlySummary() {
+export default function MonthlySummary(props) {
+
+    const { property, currencyFormatter } = props
     return (
-        <div>
-            <h3 style={styles.heading}>Monthly Summary</h3>
+        <div style={styles.container}>
+            <h3 style={styles.heading}>Monthly Estimation</h3>
             <div style={styles.summaryContainer}>
                 <div style={styles.debitContainer}>
                     <ul style={styles.list}>
-                        <li>Rental Income:</li>
+                        {property.unitNumber.map(unit => (
+                            <li key={unit._id} style={styles.listItem}>Unit {unit.number}: <div>{unit.rented ? currencyFormatter(unit.rentalIncome) : 0}</div></li>
+                        ))}
                     </ul>
                 </div>
                 <div style={styles.creditContainer}>
                     <ul style={styles.list}>
-                        <li>Morgage:</li>
+                        <li style={styles.listItem}>Morgage: <div>{currencyFormatter(property.morgage)}</div></li>
+                        <li style={styles.listItem}>Insurance: <div>{currencyFormatter(property.insurance.expense)}</div></li>
+                        <li style={styles.listItem}>Property Manager: <div>{currencyFormatter(property.propertyManager.expense)}</div></li>
+                        <li style={styles.listItem}>Average Expenses: <div>{currencyFormatter(property.averageExpense)}</div></li>
+                    </ul>
+                </div>
+            </div>
+            <div style={styles.totalsContainer}>
+                <div style={styles.debitTotal}>
+                    <ul>
+                        <li style={styles.listItem}>Total Rental Income: <div style={styles.totals}>{currencyFormatter(property.totalRentalIncome)}</div></li>
+                        <li style={styles.listItem}>CASHFLOW: <div style={styles.totals}>{currencyFormatter(property.cashflow)}</div></li>
+                    </ul>
+                </div>
+                <div style={styles.creditTotal}>
+                    <ul>
+                        <li style={styles.listItem}>Total Estimated Expenses: <div style={styles.totals}>{currencyFormatter(property.estimatedMonthlyExpense)}</div></li>
                     </ul>
                 </div>
             </div>
@@ -21,6 +41,9 @@ export default function MonthlySummary() {
 }
 
 const styles = {
+    container: {
+        borderBottom: '1px solid black'
+    },
     heading: {
         textAlign: 'center'
     },
@@ -31,9 +54,30 @@ const styles = {
         listStyle: 'none'
     },
     debitContainer: {
-        flex: 1
+        flex: 1,
+        borderRight: '1px solid black'
     },
     creditContainer: {
         flex: 1
+    },
+    listItem: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginBottom: '10px',
+        marginRight: '10px'
+    },
+    totalsContainer: {
+        display: 'flex'
+    },
+    debitTotal: {
+        flex: 1,
+        borderRight: '1px solid black'
+    },
+    creditTotal: {
+        flex: 1
+    },
+    totals: {
+        fontWeight: 'bold',
+        borderTop: '1px solid black'
     }
 }
--- src/components/property/PropertyProfile.js
+++ src/components/property/PropertyProfile.js
@@ -28,11 +28,6 @@ export default class PropertyProfile extends Component {
         })
     }
 
-    showDate = (date) => {
-        const year = date.getFullYear()
-        console.log(year)
-    }
-
     currencyFormatter = (number) => {
         let formatter = new Intl.NumberFormat('en-US', {
             style: 'currency',
@@ -42,19 +37,84 @@ export default class PropertyProfile extends Component {
         return formattedNumber
     }
 
-    componentWillMount() {  //This should be componentDidMount
+    getTotalRentalIncome = () => {
+        let totalIncome = 0
+        this.state.property.unitNumber.map(unit => {
+            if(unit.rented) {
+                totalIncome += unit.rentalIncome
+            } 
+            return totalIncome
+        })
+        const totalRentalIncome = totalIncome
+        return totalRentalIncome
+    }
+
+    getAverageExpense = () => {
+        let totalExpense = 0
+        this.state.property.expenses.map(expense => {
+            totalExpense += expense.expense
+            return totalExpense
+        })
+        const dateDiff = Date.now() - Date.parse(this.state.property.purchaseDate)
+        const monthsPropertyOwned = Math.floor(dateDiff / (24 * 3600 * 1000 * 30 ))
+        const averageExpense = totalExpense/monthsPropertyOwned
+        return averageExpense
+    }
+
+    getEstimatedMonthlyExpense = () => {
+        const estimatedMonthlyExpense = this.state.property.morgage + this.state.property.insurance.expense + this.state.property.propertyManager.expense + this.state.property.averageExpense
+        return estimatedMonthlyExpense
+    }
+
+    getCashflow = () => {
+        const cashflow = this.state.property.totalRentalIncome - this.state.property.estimatedMonthlyExpense
+        return cashflow
+    }
+
+    updateApi = (data) => {
+        const url = `http://localhost:3000/property/${this.state.property._id}/update`
+        fetch(url, {
+            method: 'PATCH',
+            cors: 'CORS',
+            headers: {
+                'Content-Type': 'Application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('Component Did Update')
+        const averageExpense = this.getAverageExpense()
+        const totalRentalIncome = this.getTotalRentalIncome()
+        const estimatedMonthlyExpense = this.getEstimatedMonthlyExpense()
+        const cashflow = this.getCashflow()
+        if(prevState.property.averageExpense !== averageExpense ||
+            prevState.property.totalRentalIncome !== totalRentalIncome || 
+            prevState.property.estimatedMonthlyExpense !== estimatedMonthlyExpense ||
+            prevState.property.cashflow !== cashflow) {
+            this.setState(prevState => ({
+                property: {
+                    ...prevState.property,
+                    totalRentalIncome,
+                    averageExpense,
+                    estimatedMonthlyExpense,
+                    cashflow
+                }
+            }))
+        } 
+        this.updateApi(this.state.property)
+    }
+
+    UNSAFE_componentWillMount() {  //This should be componentDidMount
         this.getPropery()
     }
 
-    test = () => {
-        console.log(this.state.property.address)
-    }
-    
     render() {
         if (!this.state.loaded) {
             return <div>LOADING...</div>
         }
-        this.test()
+
         const { property } = this.state
         return (
             <div style={ styles.constainer }>
@@ -65,11 +125,11 @@ export default class PropertyProfile extends Component {
                         </Link>
                     </div>
                     <div style={ styles.profileHeader }>
-                        <h1>{property.address.street} Profile</h1>
+                        <h1>{property.address.street }  {property.address.city}</h1>
                     </div>
                 </div>
                 <PropertyInfo property={ property } currencyFormatter={ this.currencyFormatter }/>
-                <MonthlySummary />
+                <MonthlySummary property={ property } currencyFormatter={ this.currencyFormatter }/>
             </div>
         )
     }
